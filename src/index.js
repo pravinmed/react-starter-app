2f92233800ef4afcbee5c81f4034d9380e9dead6
pravinmed <somayaji1979@gmail.com>
2019-12-24 18:08:25 -0800

    Search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './ListBooks'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>, document.getElementById('root'));
+
+//registerServiceWorker();
+  
+
+
+  
